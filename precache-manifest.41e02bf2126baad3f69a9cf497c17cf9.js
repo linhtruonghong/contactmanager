534290947c67e56eb37e29745a707c74e7870990
Linh.Truong <Linh.Hong@cosector.com>
2020-01-28 17:08:57 +0700

    Updates

---
+++ precache-manifest.41e02bf2126baad3f69a9cf497c17cf9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d24bff3c11999ae19fcb343309aae1f3",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "9a1ae62bcb7e532661c8",
+    "url": "/contactmanager/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "e55ec036b0eedbcc6da6",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "9a1ae62bcb7e532661c8",
+    "url": "/contactmanager/static/js/2.b036bdd0.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/contactmanager/static/js/2.b036bdd0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e55ec036b0eedbcc6da6",
+    "url": "/contactmanager/static/js/main.6bc3ce09.chunk.js"
+  },
+  {
+    "revision": "d86057960233a7428d99",
+    "url": "/contactmanager/static/js/runtime-main.2ce66dc9.js"
+  }
+]);
